e501ed5021f1d1daf71e319de28b8ad1037bdcd0
hyNicole <ncrowley@hylaine.com>
2022-02-28 19:47:08 -0500

    Trying in script now

--- module/db.js
+++ module/db.js
@@ -1,5 +1,14 @@
 const newman = require('newman');
-
 module.exports.init = function () {
   console.log('hi');
+  newman.run({
+    collection: require('./sample-collection.json'),
+    reporters: 'cli'
+}, function (err) {
+	if (err) { throw err; }
+    console.log('collection run complete!');
+})
+.on('beforeRequest', function (err, args) {
+    console.log(args.request.body.raw);
+});
 };
