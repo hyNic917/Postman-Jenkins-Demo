d429d34890c2201ff29526581edc08bb77778bfe
hyNicole <ncrowley@hylaine.com>
2022-03-01 18:02:49 -0500

    Quick fix to get production working

--- dbConsole/Program.cs
+++ dbConsole/Program.cs
@@ -38,7 +38,7 @@ namespace dbConsole
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{environmentName}.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environmentName ?? "Production"}.json", optional: false, reloadOnChange: true)
                 .AddUserSecrets<App>();
 
             return builder.Build();
