cef65438fecee5987ae3af96dd98697b7e179a29
hyNicole <ncrowley@hylaine.com>
2022-03-01 11:51:47 -0500

    Trying csv reporter

--- scor-tci-writer/index.js
+++ scor-tci-writer/index.js
@@ -4,20 +4,20 @@ module.exports.runNewman = function () {
   console.log('hi');
   newman.run({
     collection: require('../Petstore.postman_collection.json'),
-    reporters: ['cli', 'json', 'htmlextra']
+    reporters: ['cli', 'json', 'htmlextra', 'newman-reporter-csv']
 }, function (err) {
 	if (err) { throw err; }
     console.log('collection run complete!');
 })
 .on('beforeRequest', function (err, o) {
     console.log(o.item.name);	
-})
-.on("beforeDone", function() {
-    var report = _.get(newman, "summary.run.executions"),
-        collection = _.get(newman, "summary.collection"),
-        cache,
-        collName;
-
-    console.log(report);
 });
+// .on("beforeDone", function() {
+//     var report = _.get(newman, "summary.run.executions"),
+//         collection = _.get(newman, "summary.collection"),
+//         cache,
+//         collName;
+
+//     console.log(report);
+// });
 };
